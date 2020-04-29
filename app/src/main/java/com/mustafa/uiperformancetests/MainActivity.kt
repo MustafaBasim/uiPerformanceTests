f253e2963288d543ad009ef94c7621660fe5a2ea
Mustafa Basim <96.mustafa.basim@gmail.com>
2020-04-29 04:34:13 +0300

    added xml activity

--- app/src/main/java/com/mustafa/uiperformancetests/MainActivity.kt
+++ app/src/main/java/com/mustafa/uiperformancetests/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.mustafa.uiperformancetests
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mustafa.uiperformancetests.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.apply {
+            setContentView(root)
+            xmlBtn.setOnClickListener {
+                startActivity(Intent(root.context, XMLActivity::class.java))
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/mustafa/uiperformancetests/XMLActivity.kt
@@ -0,0 +1,11 @@
+package com.mustafa.uiperformancetests
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class XMLActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_x_m_l)
+    }
+}
